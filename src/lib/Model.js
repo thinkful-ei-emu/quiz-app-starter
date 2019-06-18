464b9ae9ef15297136c06be1c77f01ccbf3e22a2
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-06-18 16:46:35 -0400

    Adding additional comments to Model.js

--- src/lib/Model.js
+++ src/lib/Model.js
@@ -12,16 +12,23 @@ class Model {
   constructor() {
     // creates hidden "view registry" where the model may be bound to multiple views
     // A WeakMap is collection of key/value pairs in which the keys must be objects and the values can be arbitrary values. A WeakMap is NOT iterable.
+    // The key is going to be the model (Quiz)
+    // the value is an Array of the views (QuizDisplay & QuizStatus)
     if (!Model.viewMap) Model.viewMap = new WeakMap();
+    // this === instance of Quiz
     Model.viewMap.set(this, []);
     console.log(Model.viewMap);
   }
-
+  /**
+   * Used in Renderer class to push the views onto the WeakMap:
+   * this.model.bindView(this);
+   * @param {Class} view 
+   */
   bindView(view) {
     const views = Model.viewMap.get(this);
     views.push(view);
   }
-  // automatically runs a render cycle
+  // automatically runs a render cycle for each view
   update() {
     const views = Model.viewMap.get(this);
     if (!views || views.length === 0) return;
